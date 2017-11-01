f73600955b70dc8b3617922b9a7636841c3b814a
Tahsin Kasap <tahsin@ovidos.com>
2017-11-01 16:35:17 +0300

    Initial Commit

---
+++ Ovidos.WebHooks.Facebook/Extensions/HttpConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+using Microsoft.AspNet.WebHooks.Config;
+
+// ReSharper disable once CheckNamespace
+namespace System.Web.Http
+{
+    /// <summary>
+    /// Extension methods for <see cref="HttpConfiguration"/>.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class HttpConfigurationExtensions
+    {
+        /// <summary>
+        /// Initializes support for receiving Facebook WebHooks.
+        /// Set the '<c>MS_WebHookReceiverSecret_Facebook</c>' application setting to the application secrets, optionally using IDs
+        /// to differentiate between multiple WebHooks, for example '<c>secret0, id1=secret1, id2=secret2</c>'.
+        /// The corresponding WebHook URI is of the form '<c>https://&lt;host&gt;/api/webhooks/incoming/facebook/{id}</c>'.
+        /// </summary>
+        /// <param name="config">The current <see cref="HttpConfiguration"/>config.</param>
+        public static void InitializeReceiveGitHubWebHooks(this HttpConfiguration config)
+        {
+            WebHooksConfig.Initialize(config);
+        }
+    }
+}
