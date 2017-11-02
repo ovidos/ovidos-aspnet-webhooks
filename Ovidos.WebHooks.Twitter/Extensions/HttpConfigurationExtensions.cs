c6b71b7857f898695e5733ce828505a71f7c3898
Tahsin Kasap <tahsin@ovidos.com>
2017-11-02 15:53:07 +0300

    Twitter WebHook Initial Commit

--- Ovidos.WebHooks.Twitter/Extensions/HttpConfigurationExtensions.cs
+++ Ovidos.WebHooks.Twitter/Extensions/HttpConfigurationExtensions.cs
@@ -11,13 +11,13 @@ namespace System.Web.Http
     public static class HttpConfigurationExtensions
     {
         /// <summary>
-        /// Initializes support for receiving Facebook WebHooks.
-        /// Set the '<c>MS_WebHookReceiverSecret_Facebook</c>' application setting to the application secrets, optionally using IDs
+        /// Initializes support for receiving Twitter WebHooks.
+        /// Set the '<c>MS_WebHookReceiverSecret_Twitter</c>' application setting to the application secrets, optionally using IDs
         /// to differentiate between multiple WebHooks, for example '<c>secret0, id1=secret1, id2=secret2</c>'.
-        /// The corresponding WebHook URI is of the form '<c>https://&lt;host&gt;/api/webhooks/incoming/facebook/{id}</c>'.
+        /// The corresponding WebHook URI is of the form '<c>https://&lt;host&gt;/api/webhooks/incoming/twitter/{id}</c>'.
         /// </summary>
         /// <param name="config">The current <see cref="HttpConfiguration"/>config.</param>
-        public static void InitializeReceiveFacebookWebHooks(this HttpConfiguration config)
+        public static void InitializeReceiveTwitterWebHooks(this HttpConfiguration config)
         {
             WebHooksConfig.Initialize(config);
         }
